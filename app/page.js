c4527f0c448c82ffcc937e00a8757361545f6f61
Asif Ansari <asifstudy123@gmail.com>
2025-09-18 01:37:31 +0530

    updated home screen

--- app/page.js
+++ app/page.js
@@ -1,13 +1,27 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-
   const router = useRouter();
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Button className={'hover:cursor-pointer'} onClick={()=>router.replace('/dashboard')}>Start your Mock interview with Ai</Button>
+    <div className="h-screen flex flex-col lg:flex-row bg-gray-50 overflow-hidden ">
+      <div>
+        <img
+          src="/Online-interview.jpg"
+          alt="page.jpg"
+          className="w-full h-full object-cover "
+        />
+      </div>
+
+      <div className="flex justify-center items-center h-screen ml-20">
+        <Button
+          className={"hover:cursor-pointer hover:bg-blue-600"}
+          onClick={() => router.replace("/dashboard")}
+        >
+          Start your Mock interview with Ai
+        </Button>
+      </div>
     </div>
   );
 }
